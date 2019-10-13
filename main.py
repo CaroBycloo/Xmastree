921f5070eb3ff0a44959b1df32faa59940c0b1ed
CaroBycloo <caro.merlier@gmail.com>
2019-10-13 21:30:42 +0200

    draw tree parts

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ def main():
     dimension = 10
     height = 4
     width = 5
-    drawTreeBody(dimension)
+    drawTreeBodyPart(6, 0, 4)
+    drawTreeBodyPart(3, 2, 7)
+    drawTreeBodyPart(0, 5, 10)
     drawTreeTrunk(dimension, height, width)
 
-def drawTreeBody(dimension):
-    spaces = dimension - 1
-    symbol = 1
+def drawTreeBodyPart(offset, startLine, endLine):
+    spaces = endLine - 1 - startLine + offset
+    numberOfSymbols = 1 + startLine * 2
     
-    while spaces >= 0:
-        line = (' ' * spaces) + ('+' * symbol)
+    while spaces >= offset:
+        line = (' ' * spaces) + ('+' * numberOfSymbols)
         spaces = spaces - 1
-        symbol = symbol + 2
+        numberOfSymbols = numberOfSymbols + 2
         print(line)
 
 def drawTreeTrunk(dimension, height, width):
